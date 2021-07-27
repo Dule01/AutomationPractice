302d88883486f6962aa4e7fae6037c6d85f7ac08
Dusan <simic.dusan95@gmail.com>
2021-07-27 16:12:34 +0200

    removed comments

--- src/test/java/tests/AutomationPracticeTest.java
+++ src/test/java/tests/AutomationPracticeTest.java
@@ -15,69 +15,69 @@ public class AutomationPracticeTest extends BaseTest {
         init(browser, 15);
     }
 
-//    @Test(description = "Verify functionality of the 'Contact Us' button")
-//    @Description("The 'Contact Us' button properly works and navigates the user to the Contact Page")
-//    public void verifyContactUsBtn(){
-//        driver.get("http://automationpractice.com/index.php");
-//        BasePage bp = new BasePage(driver);
-//        bp.verifyContactUsBtn();
-//    }
-//
-//    @Test(description = "Verify functionality of the 'Sign In' button")
-//    @Description("The 'Sign In' button properly works and navigates the user to the Sign In Page")
-//    public void verifySignInBtn(){
-//        driver.get("http://automationpractice.com/index.php");
-//        BasePage bp = new BasePage(driver);
-//        bp.verifySignInBtn();
-//    }
-//
-//    @Test(description = "Verify functionality of the footer Information links")
-//    @Description("Links under the 'Information' category at the bottom of the screen properly function")
-//    public void verifyFooterInfoLinks(){
-//        driver.get("http://automationpractice.com/index.php");
-//        BasePage bp = new BasePage(driver);
-//        bp.selectFooterInformationLink("Sitemap");
-//    }
-//
-//    @Test(description = "Verify functionality of the footer My Account links")
-//    @Description("Links under the 'My Account' category at the bottom of the screen properly function")
-//    public void verifyFooterMyAccLinks(){
-//        driver.get("http://automationpractice.com/index.php");
-//        BasePage bp = new BasePage(driver);
-//        bp.selectFooterMyAccountLink("My orders");
-//    }
-//
-//    @Test(description = "Verify Newsletter Bar")
-//    @Description("The Newsletter Bar properly functions after entering a valid email address")
-//    public void verifyNewsletter(){
-//        driver.get("http://automationpractice.com/index.php");
-//        BasePage bp = new BasePage(driver);
-//        bp.enterNewsletterEmail("testemail"+System.currentTimeMillis()+"@email.com");
-//    }
-//
-//    @Test(description = "Verify social media buttons")
-//    @Description("All of the social media buttons properly function")
-//    public void socialButtons(){
-//        driver.get("http://automationpractice.com/index.php");
-//        BasePage bp = new BasePage(driver);
-//        bp.checkSocialButtons();
-//    }
-//
-//    @Test(description = "Verify email link")
-//    @Description("Footer email link is functional")
-//    public void emailLink(){
-//        driver.get("http://automationpractice.com/index.php");
-//        BasePage bp = new BasePage(driver);
-//        bp.verifyEmail();
-//    }
+    @Test(description = "Verify functionality of the 'Contact Us' button")
+    @Description("The 'Contact Us' button properly works and navigates the user to the Contact Page")
+    public void verifyContactUsBtn(){
+        driver.get("http://automationpractice.com/index.php");
+        BasePage bp = new BasePage(driver);
+        bp.verifyContactUsBtn();
+    }
 
-//    @Test(description = "Verify text for invalid email")
-//    @Description("Error text for an invalid email is valid")
-//    public void invalidEmailNewsletter(){
-//        driver.get("http://automationpractice.com/index.php");
-//        BasePage bp = new BasePage(driver);
-//        bp.enterInvalidNewsletterEmail("invalidemail");
-//    }
+    @Test(description = "Verify functionality of the 'Sign In' button")
+    @Description("The 'Sign In' button properly works and navigates the user to the Sign In Page")
+    public void verifySignInBtn(){
+        driver.get("http://automationpractice.com/index.php");
+        BasePage bp = new BasePage(driver);
+        bp.verifySignInBtn();
+    }
+
+    @Test(description = "Verify functionality of the footer Information links")
+    @Description("Links under the 'Information' category at the bottom of the screen properly function")
+    public void verifyFooterInfoLinks(){
+        driver.get("http://automationpractice.com/index.php");
+        BasePage bp = new BasePage(driver);
+        bp.selectFooterInformationLink("Sitemap");
+    }
+
+    @Test(description = "Verify functionality of the footer My Account links")
+    @Description("Links under the 'My Account' category at the bottom of the screen properly function")
+    public void verifyFooterMyAccLinks(){
+        driver.get("http://automationpractice.com/index.php");
+        BasePage bp = new BasePage(driver);
+        bp.selectFooterMyAccountLink("My orders");
+    }
+
+    @Test(description = "Verify Newsletter Bar")
+    @Description("The Newsletter Bar properly functions after entering a valid email address")
+    public void verifyNewsletter(){
+        driver.get("http://automationpractice.com/index.php");
+        BasePage bp = new BasePage(driver);
+        bp.enterNewsletterEmail("testemail"+System.currentTimeMillis()+"@email.com");
+    }
+
+    @Test(description = "Verify social media buttons")
+    @Description("All of the social media buttons properly function")
+    public void socialButtons(){
+        driver.get("http://automationpractice.com/index.php");
+        BasePage bp = new BasePage(driver);
+        bp.checkSocialButtons();
+    }
+
+    @Test(description = "Verify email link")
+    @Description("Footer email link is functional")
+    public void emailLink(){
+        driver.get("http://automationpractice.com/index.php");
+        BasePage bp = new BasePage(driver);
+        bp.verifyEmail();
+    }
+
+    @Test(description = "Verify text for invalid email")
+    @Description("Error text for an invalid email is valid")
+    public void invalidEmailNewsletter(){
+        driver.get("http://automationpractice.com/index.php");
+        BasePage bp = new BasePage(driver);
+        bp.enterInvalidNewsletterEmail("invalidemail");
+    }
 
     @Test(description = "Verify text for repeated email")
     @Description("Error text for an already used email is valid")
